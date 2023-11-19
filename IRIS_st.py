d0361521ce57ccc919c23cb67de08aa66d871811
Manoj hari <92537653+ManojkumarVH@users.noreply.github.com>
2023-11-20 00:34:31 +0530

    Update IRIS_st.py

--- IRIS_st.py
+++ IRIS_st.py
@@ -7,6 +7,28 @@ st.title('**IRIS PREDICTION APP 🌸**')
 st.markdown("""
 - *This app predict iris flower type* 
 """)
+st.subheader('Iris Genus')
+st.markdown(""" - *Iris Genus:The Iris genus encompasses a diverse group of flowering plants known for their stunning and varied blooms.
+Iris flowers are characterized by their unique petal patterns and come in a wide array of colors.
+The genus includes both herbaceous plants and bulbous species. Irises are widely cultivated for their ornamental value
+and are symbolic of beauty and connection *""")
+st.subheader('Iris setosa')
+st.markdown("""
+- Iris setosa is one of the three species in the Iris genus. It is known for its distinctive appearance,
+characterized by short, sturdy stems and showy flowers with petals that can be white, blue, or pink.
+Habitat: Found in cold regions, including parts of North America and Europe.""")
+st.subheader('Iris versicolor')
+st.markdown("""
+- Iris versicolor, also known as the "Harlequin Blue flag," displays a wide range of colors, including blue, 
+violet, and occasionally pink. It is recognized for its striking appearance and can be found in wetland areas.
+Habitat: Native to North America, particularly in wetlands and along the edges of ponds.""")
+st.subheader('Iris virginica')
+st.markdown("""
+- Iris virginica, commonly known as the "Virginia Iris" or "Southern Blue Flag," 
+features blue to violet flowers with distinctive veining on the petals.
+It is a wetland-loving species and is recognized for its elegance.
+Habitat: Native to the eastern United States and typically found in marshy areas.
+""")
 st.sidebar.header(' input parameters')
 
 
